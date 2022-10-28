e1a3b245d6ded06d5dea58ac99138a7d5cfbdcbf
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-28 18:57:10 +0300

    completed phase 3 sql select and update

---
+++ 01_insert_cats_into_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 5, "American Shorthair");
+INSERT INTO cats (name, age, breed) VALUES ("Hannah", 5, "Tabby");
+INSERT INTO cats (name, age, breed) VALUES ("Hannah", 5, "Tabby");
